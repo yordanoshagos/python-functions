b31969f08c33760b05ec25f73bcbec2fbae2837a
Yordanos Tesfay <yordanostesfayhagos@gmail.com>
2025-04-24 20:53:48 +0300

    add python functions

---
+++ index.py
@@ -0,0 +1,26 @@
+def square(number):
+    print(number*number)
+
+def add(x,y):
+    result = x+y
+    return result
+
+def multiply(x,y,z):
+    result = x*y*z
+    return result 
+
+def divide(x,y):
+    result =  x/y
+    return result 
+
+def subtract(x,y):
+    result = x-y 
+    return result 
+
+def exponent(x,y):
+    result = x**y
+    return result
+
+def modulus(x,y):
+    result = x%y
+    return result
